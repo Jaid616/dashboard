fbfb6ef44b06404ae07ea38b605866c139ae6651
Jaid616 <78507633+Jaid616@users.noreply.github.com>
2023-02-22 14:19:51 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,24 +19,24 @@ const Footer = () => {
         style={{ backgroundColor: "#9c27b0", marginTop: "10px" }}
       >
         <Stack direction="row" spacing={1}>
-          <IconButton aria-label="twitter" style={{ color: "#ffffff" }}>
+          <IconButton aria-label="twitter" style={{ color: "#ffffff" }}  component = "a" href = "https://twitter.com/jaidnasim1">
             <TwitterIcon />
           </IconButton>
-          <IconButton aria-label="facebook" style={{ color: "#ffffff" }}>
+          <IconButton aria-label="facebook" style={{ color: "#ffffff" }}  component = "a" href = "https://www.facebook.com/jaid.nasim.39">
             <FacebookIcon />
           </IconButton>
-          <IconButton aria-label="github" style={{ color: "#ffffff" }}>
+          <IconButton aria-label="github" style={{ color: "#ffffff" }}  component = "a" href = "https://github.com/Jaid616">
             <GitHubIcon />
           </IconButton>
-          <IconButton aria-label="linkedin" style={{ color: "#ffffff" }}>
+          <IconButton aria-label="linkedin" style={{ color: "#ffffff" }}  component = "a" href = "https://www.linkedin.com/in/jaid-nasim-148113240">
             <LinkedInIcon />
           </IconButton>
-          <IconButton aria-label="instagram" style={{ color: "#ffffff" }}>
+          <IconButton aria-label="instagram" style={{ color: "#ffffff" }}  component = "a" href = "https://www.instagram.com/jaidnasim">
             <InstagramIcon />
           </IconButton>
         </Stack>
         <Typography variant="subtitle2" color="#ffffff">
-          © Jaid Nasim 2022
+          © Jaid Nasim 2023
         </Typography>
       </Stack>
     </>
